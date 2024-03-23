352539091ca903664dfdb2f1f46892a3b7b67bd3
Beirão <phsicopt@gmail.com>
2024-03-23 05:48:31 +0000

    Fixed BUFFER_SIZE

--- Files/get_next_line.h
+++ Files/get_next_line.h
@@ -17,9 +17,7 @@
 # include <stdio.h>
 # include <fcntl.h>
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 42
-# endif
+# define BUFFER_SIZE 42
 
 char	*get_next_line(int fd);
 char	*ft_strjoin(char const *s1, char const *s2);
