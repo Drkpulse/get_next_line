37fb285540d2887404f95c7a5df9c384d91da2d8
Erik Gabriel <32306841+Err-G@users.noreply.github.com>
2024-04-18 19:56:13 +0100

    correct buffer_size guard

--- Files/get_next_line_bonus.h
+++ Files/get_next_line_bonus.h
@@ -17,7 +17,9 @@
 # include <stdio.h>
 # include <fcntl.h>
 
-# define BUFFER_SIZE 42
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
 char	*get_next_line(int fd);
 char	*ft_strjoin(char const *s1, char const *s2);
